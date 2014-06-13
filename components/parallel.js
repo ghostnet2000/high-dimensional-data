4a7c6ebb5b6a55547a810ad2c167dcde0abb3bbd
Viwe <viwe@ubuntu.(none)>
2014-06-13 12:29:37 +0200

    parallel coordinate created on components"

---
+++ components/parallel.js
@@ -0,0 +1,26 @@
+
+function parallel(data){
+	parallel = {}
+
+	function parallel.setup = function(height, width, padding, mainDiv) {
+		// body...
+		this.height = height
+		this.width = width
+		this.padding = padding
+
+		return parallel
+	}
+
+	function parallel.plot = function(mainDiv) {
+		// body...
+		return parallel
+	}
+
+	function parallel.recolour = function() {
+		//body...
+
+		return parallel
+	}
+
+	return parallel
+}
