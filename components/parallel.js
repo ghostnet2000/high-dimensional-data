c276a7f1061cd6b84e15e6ded839af8ea5a16716
Viwe <viwelolwane@gmail.com>
2014-06-23 12:01:21 +0200

    minor changes - changed logic - fixed some minor errors

--- components/parallel.js
+++ components/parallel.js
@@ -1,47 +1,57 @@
+function setupParallel(height, width, padding, mainDiv) {
+    var favColour = "steelblue"
 
-function parallel(data){
-	parallel = {}
+    // scales and scaled-getters
+    var o = getScales([height, 0]);
+    var scale = o.scale,
+        scaleGet = o.scaleGet;
 
-	function parallel.setup = function(height, width, padding, mainDiv) {
-		this.height = height
-		this.width = width
-		this.padding = padding
+    // chart
+    var eachWidth = width / (data.traits.length - 1)
+    var parallel = d3.select(mainDiv)
+        .append("svg")
+        .attr("class", "chart")
+        .attr("width", width + 2*padding)
+        .attr("height", height + 2*padding);
 
-		return parallel
-	}
+    // axes
+    var axes = d3.svg.axis()
+        .ticks(4)
+        .tickSize(1)
 
-	function parallel.plot = function(mainDiv) {
-		var eachWidth = this.width / (data.traits.length - 1)
-		this.chart = d3.select(mainDiv).html("")
-			.append("svg")
-			.attr("class", "chart")
-			.attr("width", this.width + 2*this.padding)
-			.attr("height", this.height + 2*this.padding);
+    parallel.selectAll("g.axis")
+        .data(data.traits)
+        .enter().append("svg:g")
+        .attr("class", "y axis")
+        .attr("transform",
+              function(d, i) {
+                return "translate("+((i*eachWidth)+padding)+","+padding+")"
+              })
+        .each(function(d){
+          d3.select(this).call(axes.scale(scale[d]).orient("left"));
+        });
 
-		for (var i=1; i< data.traits.length; i++) {
-			var prev = data.traits[i-1],
-				current = data.traits[i]
-			parallel.chart.selectAll("linechart")
-				.data(data.values)
-				.enter().append("svg:line")
-				.attr("transform", "translate(0,"+this.padding+")")
-				.attr("class", "path")
-				.attr("x1", this.padding + (i-1)*eachWidth)
-				.attr("y1", function(d) { return parallel.o.scaleGet[prev](d) })
-				.attr("x2", this.padding + i*eachWidth)
-				.attr("y2", function(d) { return parallel.o.scaleGet[current](d) })
-				.attr("width", 1)
-				.style("stroke-opacity", 0.5)
-				.style("stroke", colour.point)
-	    }
-	    return parallel
-	}
 
-	function parallel.recolour = function() {
-		parallel.chart.selectAll("line.path").style("stroke", colour.point)
-		return parallel
-	}
+     // lines
+     var lines = parallel.selectAll("g.line")
+         .data(data.values)
+         .enter().append("svg:g")
+         .attr("transform", "translate(0,"+padding+")")
+         .each (plotLines)
 
-	return parallel
+    function plotLines(point) {
+      var lines = d3.select(this)
+      for (var i = 0; i < data.traits.length - 1; i++) {
+        current = data.traits[i]
+        next = data.traits[i+1]
+
+        lines.append("svg:line")
+            .attr("x1", padding + i*eachWidth)
+            .attr("y1", scaleGet[current](point))
+            .attr("x2", padding + (i+1)*eachWidth)
+            .attr("y2", scaleGet[next](point))
+            .attr("width", 1)
+            .attr("stroke", favColour)
+      }
+    }
 }
-
--- data/randomdata.js
+++ data/randomdata.js
@@ -4,17 +4,39 @@ function randpoint() {
         x : Math.random()*7,
         y : Math.random()*7,
         z : Math.random()*6,
-        t : Math.random()*7,
+        t : Math.random()*7
     }
 }
 
 random = {}
-
 // getter function
+/*
 function getData()
 {
 	return {
-		'traits': ['z', 'y', 'z', 't' ],
-		'values': d3.range(500).map(randpoint)
+		'traits': ['x', 'y', 'z', 't' ],
+		'values': d3.range(50).map(randpoint)
 	};
+}
+*/
+var data = {
+	'traits': ['x', 'y', 'z', 't'],
+	'category': 1,
+	'values' : d3.range(25).map(randpoint)
+};
+
+// getter and scale
+function getScales(range) {
+	var o = {
+		scale: {},
+		scaleGet: {}
+	}
+
+	data.traits.forEach(function(trait) {
+		getter = function(d) { return d[trait] }
+		domain = [d3.min(data.values, getter), d3.max(data.values, getter)]
+		o.scale[trait] = d3.scale.linear().domain(domain).range(range)
+		o.scaleGet[trait] = function(d) { return o.scale[trait](d[trait]) }
+	});
+	return o
 }
